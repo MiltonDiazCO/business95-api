608ee716e4494a2ebd40a84b21802d0d4c4313d5
MiltonDiazCO <diazmartinezmilton@gmail.com>
2025-07-16 20:19:05 -0500

    El endpoint POST de movimientos ahora retorna ResponseEntity 201

--- src/main/java/com/business95/api/business95_api/controllers/MovimientoController.java
+++ src/main/java/com/business95/api/business95_api/controllers/MovimientoController.java
@@ -3,6 +3,9 @@ package com.business95.api.business95_api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,8 +29,8 @@ public class MovimientoController {
     }
 
     @PostMapping
-    public MovimientoDTO registrarMovimiento(@RequestBody MovimientoDTO movimientoDTO) {
-        return movimientoService.save(movimientoDTO);
+    public ResponseEntity<MovimientoDTO> registrarMovimiento(@RequestBody MovimientoDTO movimientoDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(movimientoService.save(movimientoDTO));
     }
 
 }

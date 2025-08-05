f080b6d183e44fdf4368c26a88e16ce29b6ac93e
MiltonDiazCO <diazmartinezmilton@gmail.com>
2025-08-04 20:19:06 -0500

    Endpoint para la consulta de medidas

---
+++ src/main/java/com/business95/api/business95_api/servicies/implementations/MedidaServiceImpl.java
@@ -0,0 +1,23 @@
+package com.business95.api.business95_api.servicies.implementations;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.business95.api.business95_api.entities.Medida;
+import com.business95.api.business95_api.repositories.MedidaRepository;
+import com.business95.api.business95_api.servicies.interfaces.MedidaService;
+
+@Service
+public class MedidaServiceImpl implements MedidaService {
+
+    @Autowired
+    private MedidaRepository medidaRepository;
+
+    @Override
+    public List<Medida> findAll() {
+        return (List<Medida>) medidaRepository.findAll();
+    }
+
+}
---
+++ src/main/java/com/business95/api/business95_api/servicies/interfaces/MedidaService.java
@@ -0,0 +1,11 @@
+package com.business95.api.business95_api.servicies.interfaces;
+
+import java.util.List;
+
+import com.business95.api.business95_api.entities.Medida;
+
+public interface MedidaService {
+
+    List<Medida> findAll();
+
+}

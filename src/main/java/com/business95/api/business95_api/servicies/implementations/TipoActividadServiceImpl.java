95f071941972f55cae56a10aa64bb11a3e291ae1
MiltonDiazCO <diazmartinezmilton@gmail.com>
2025-08-04 20:24:41 -0500

    Endpoint para la consulta de los tipos de actividades

---
+++ src/main/java/com/business95/api/business95_api/servicies/implementations/TipoActividadServiceImpl.java
@@ -0,0 +1,23 @@
+package com.business95.api.business95_api.servicies.implementations;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.business95.api.business95_api.entities.TipoActividad;
+import com.business95.api.business95_api.repositories.TipoActividadRepository;
+import com.business95.api.business95_api.servicies.interfaces.TipoActividadService;
+
+@Service
+public class TipoActividadServiceImpl implements TipoActividadService {
+
+    @Autowired
+    private TipoActividadRepository tipoActividadRepository;
+
+    @Override
+    public List<TipoActividad> findAll() {
+        return (List<TipoActividad>) tipoActividadRepository.findAll();
+    }
+
+}
